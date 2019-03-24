860932f7eb7d0f77ff5ca91d0c7d5b75f4a9a521
stolenng <stolenng@gmail.com>
2019-03-24 10:04:56 +0000

    initial mobx stores commit

---
+++ src/stores/domain-objects/yosk.js
@@ -0,0 +1,41 @@
+import { observable, computed, action} from "mobx"
+import Log from "./log";
+import Details from "./details";
+import MemoryProfiler from "./memory_profiler";
+import Queries from "./queries";
+
+
+export default class Yosk {
+    @observable details;
+    @observable log;
+    @observable memoryProfiler;
+    @observable queries;
+    @observable status;
+
+    constructor(details, log, memoryProfiler, queries) {
+        this.setYosk({
+            details,
+            log,
+            memoryProfiler,
+            queries
+        });
+    }
+
+    @action
+    setYosk(yosk) {
+        this.log = new Log(yosk.log);
+        this.details = new Details(yosk.details);
+        this.memoryProfiler = new MemoryProfiler(yosk.memoryProfiler);
+        this.queries = new Queries(yosk.queries);
+    }
+
+    @computed
+    getYosk() {
+        return {
+            log: this.log,
+            details: this.details,
+            memoryProfiler: this.memoryProfiler,
+            queryies: this.queries
+        };
+    }
+}
---
+++ src/stores/root-store.js
@@ -0,0 +1,8 @@
+import YosksStore from './domain-stores/yosk-store';
+
+class RootStore {
+    constructor() {
+        this.yosksStore = new YosksStore();
+        console.log(this, 'mobx store inited!');
+    }
+}
