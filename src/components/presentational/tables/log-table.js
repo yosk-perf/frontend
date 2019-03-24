c1f1b902e386cbcd3a56b907e8af3a2fcfa840a1
stolenng <stolenng@gmail.com>
2019-03-24 21:24:11 +0000

    Added new amazing icon

--- src/components/presentational/tables/log-table.js
+++ src/components/presentational/tables/log-table.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Table, Tag } from 'antd';
-import CustomCard from "../custom-card/custom-card";
 
 const getLogLevelColor = (logLevel) => {
   switch (logLevel) {
