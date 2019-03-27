1a24c1483af4cd39fe349a56da8750a57fa209c6
Aviv Moshe <avivmoshe96@gmail.com>
2019-03-27 15:13:07 +0200

    removed unused imports

--- src/components/presentational/nav-bar/nav-bar.js
+++ src/components/presentational/nav-bar/nav-bar.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import {inject, observer} from "mobx-react/index";
 import yoskLogo from './yosk-logo.png';
 import { Layout, Row, Col } from 'antd';
